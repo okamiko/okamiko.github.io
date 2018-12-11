ca61e72988612f5d2674ad780307959a39a085ae
root <root@Jeff.localdomain>
2018-12-10 20:48:22 -0800

    part 2/3

---
+++ cnit133/hw7/js/hw7part2.js
@@ -0,0 +1,9 @@
+function showList(list) {
+	const displayList = document.getElementById(list);
+	displayList.style.display = 'block';
+}
+
+function hideList(list) {
+	const displayList = document.getElementById(list);
+	displayList.style.display = 'none';
+}
